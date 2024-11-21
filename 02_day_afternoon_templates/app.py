47119f7239b4d26ecc41964087931a9641db8c0d
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-11-21 13:59:46 +0530

    passing data to template

--- 02_day_afternoon_templates/app.py
+++ 02_day_afternoon_templates/app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1> Welcome page </h1>'
+    return render_template('index.html', userName = 'Android')
 
-if __name__ == 'main':
+if __name__ == '__main__':
     app.run(debug = True)
 
 
