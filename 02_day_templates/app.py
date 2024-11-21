00e624d4434752971228c9a23796fce2515e6f27
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-11-21 11:55:32 +0530

    correcting template folder

--- 02_day_templates/app.py
+++ 02_day_templates/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template('hello.html', userName = 'Android')
 
+@app.route("/")
+def home():
+    return "<h1> Welcome Page </h1>"
+
 if __name__ == '__main__':
     app.run(debug = True)
