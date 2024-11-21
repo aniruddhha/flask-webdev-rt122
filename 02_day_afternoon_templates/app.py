358644559e85a9b3d27e0c3b465f9705793618f5
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-11-21 14:25:05 +0530

    template inheritance

--- 02_day_afternoon_templates/app.py
+++ 02_day_afternoon_templates/app.py
@@ -4,9 +4,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html', userName = 'Android')
+    return render_template('home.html')  # Home page extends base
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug = True, port = 8989)
 
 
