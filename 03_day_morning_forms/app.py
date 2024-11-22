9b877de16bf56cfb5bca317e977a7b19b3796de9
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-11-22 12:07:06 +0530

    form redirection

--- 03_day_morning_forms/app.py
+++ 03_day_morning_forms/app.py
@@ -1,12 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/form', methods = ['GET', 'POST'])
 def form():
-    ...
+    if request.method == 'POST':
+        email = request.form.get('email')    
+        message = request.form.get('message')
+
+        print(f'Email {email}')
+        print(f'Message {message}')
+
+        if not email or not message:
+            return render_template('error.html')
+
+    return render_template('success.html')
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug = True, port = 8989)
 
